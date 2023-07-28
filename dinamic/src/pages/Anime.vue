62d88259dd2ad2a3a886064317d85f9259f897b7
vitodent <140506272+vitodent@users.noreply.github.com>
2023-07-29 01:36:54 +0300

    try

--- dinamic/src/pages/Anime.vue
+++ dinamic/src/pages/Anime.vue
@@ -1,4 +1,72 @@
 <template>
 <h1>Anime</h1>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lotus</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+
+    <header class="header-container">
+        <div id="header-container">
+            <img id="logo" src="images/logo.png" />
+            
+            <nav class="header_item2">
+                <ul class="menu_container">
+                    <li><a href="index.html"> Anime </a></li>
+                    <li><a href="index.html"> Movies </a></li>
+                    <li><a href="index.html"> Manga </a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <h1 class="font-effect-emboss">Alege ce vrei sa urmaresti</h1>
+     <div id="images">
+    <section class="images">
+      <ul>
+        <div>
+          <a href="#"><img src="images/1.jpg" alt="1" /></a>
+          <h3 class="font-effect-emboss">Attack On Tittan  </h3>
+           <!-- Add icon library -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+         <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+        
+        </div>
+        <div>
+          <a href="#"><img src="images/3.jpg" alt="1" /> </a>
+            <h3 class="font-effect-emboss"> Your Name </h3>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+                <span class="fa fa-star checked"></span>
+                   <span class="fa fa-star checked"></span>
+                   <span class="fa fa-star checked"></span>
+                   <span class="fa fa-star checked"></span>
+                   <span class="fa fa-star checked"></span>
+        </div>
+         <div>
+          <a href="#"><img src="images/2.jpg" alt="1" /></a>
+          <h3 class="font-effect-emboss"> Death Note </h3>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+            <span class="fa fa-star checked"></span>
+               <span class="fa fa-star checked"></span>
+               <span class="fa fa-star checked"></span>
+               <span class="fa fa-star checked"></span>
+               <span class="fa fa-star checked"></span>
+          </div>
+      </ul>
+    </section>
+    </div>
+  </body>
+  <footer class="hi">
+
+  </footer>
 </template>
 <script></script>
