23269b205ee2fca846fed26ba18206d55796d2c2
☆ Asfoxyre <maffichu@gmail.com>
2023-07-29 01:38:02 +0300

    Code Fix Search Bar

--- dinamic/src/App.vue
+++ dinamic/src/App.vue
@@ -1,43 +1,5 @@
-<script setup>
+<!-- <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import { getDatabase } from "firebase/database";
-// const database = getDatabase();
-
-
-
-
-
-// import { getDatabase, ref, push, set } from "firebase/database";
-
-// // Create a new post reference with an auto-generated id
-// const db = getDatabase();
-// const postListRef = ref(db, 'posts');
-// const newPostRef = push(postListRef);
-// set(newPostRef, {
-//     // ...
-// });
-
-
-
-
-
-// import { getDatabase, ref, onChildAdded, onChildChanged, onChildRemoved } from "firebase/database";
-
-// const db = getDatabase();
-// const commentsRef = ref(db, 'post-comments/' + postId);
-// onChildAdded(commentsRef, (data) => {
-//   addCommentElement(postElement, data.key, data.val().text, data.val().author);
-// });
-
-// onChildChanged(commentsRef, (data) => {
-//   setCommentValues(postElement, data.key, data.val().text, data.val().author);
-// });
-
-// onChildRemoved(commentsRef, (data) => {
-//   deleteComment(postElement, data.key);
-// });
-
-
 </script>
 
 <template>
@@ -65,7 +27,7 @@ body {
   font-family: 'Nunito', sans-serif;
   color: aliceblue;
   margin:10px;
-  background-color: #55505C;
+  background-color: #100f11;
 }
 a {
   text-decoration: none;
@@ -76,15 +38,14 @@ a:hover {
 }
 .header-container {
   padding: 10px 0;
-  background-color:#5D737E;
+  background-color:#333235;
   color: white;
   display: flex;
   align-items: center; 
   margin: auto;
   border-radius: 50px;
-  box-shadow: 10px 10px 130px rgb(76, 68, 92);
+  box-shadow: 10px 10px 130px rgba(5, 5, 5, 0.612)
 }
-
 li {
   display: inline;
   margin-left: 80px;
@@ -113,7 +74,7 @@ ul {
   transition: all .2s ease-in-out;
 }
 .logo:hover {
-  transform: scale(1.5);
+  transform: scale(1.05);
   
 }
 .menu-item:hover {
@@ -123,6 +84,9 @@ ul {
 }
 .search-box{
   width: fit-content;
+  position: relative;
+    display: flex;
+    align-items: center;
   height: fit-content;
 }
 .input-search{
@@ -160,7 +124,8 @@ ul {
   color:#ffffff ;
   background-color:transparent;
   pointer-events: painted;
-  margin-left: 60px;
+  right: 0; /* Adjust the position to the right */
+  margin-left: auto; 
 }
 .btn-search:focus ~ .input-search{
   width: 300px;
@@ -191,10 +156,173 @@ button:hover {
 .search {
   width: 30px;
  height: auto;
- margin-right: 60px;
  margin-inline-end: auto;
+ margin-right: 0;
 }
 h1 {
   margin: 20px;
 }
+</style> -->
+<template>
+  <header class="header-container">
+    <RouterLink to="/home"><img class="logo" src="https://i.ibb.co/N1VDV6D/logo-480.png"></RouterLink>
+    <nav class="menu-container">
+      <ul>
+        <li><a class="menu-item" href="#"><router-link to="/view/movie">Movies</router-link></a></li>
+        <li><a class="menu-item" href="#"><router-link to="/view/anime">Anime</router-link></a></li>
+        <li><a class="menu-item" href="#"><router-link to="/view/manga">Manga</router-link></a></li>
+      </ul>
+    </nav>
+    <div class="search-box">
+      <input
+        type="text"
+        class="input-search"
+        :class="{ 'input-expanded': isSearchExpanded }"
+        placeholder="   Type to Search..."
+      />
+      <button class="btn-search" @click="toggleSearch">
+        <img class="search" src="https://icons.veryicon.com/png/o/miscellaneous/prototyping-tool/search-bar-01.png" />
+      </button>
+    </div>
+  </header>
+  <br>
+  <RouterView />
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  body {
+    font-family: 'Nunito', sans-serif;
+    color: aliceblue;
+    background-color: black;
+  }
+  a {
+    text-decoration: none;
+    color: rgb(221, 222, 222);
+  }
+  a:hover {
+    color: rgb(42, 88, 196);
+  }
+  .header-container {
+    padding: 20px 0;
+    background-color:rgb(22, 22, 22);
+    color: white;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    border-radius: 50px;
+    box-shadow: 10px 10px 130px black;
+  }
+  li {
+    display: inline;
+    margin-left: 80px;
+    position: relative;
+    font-size: 20px;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+  .menu-item {
+    position: relative;
+    padding: 2px;
+    border: 2px solid transparent;
+    transition: font-size 0.2s;
+  }
+  .menu-container {
+    height: auto;
+    width: 50px;
+    flex-grow: 1;
+  }
+  .logo {
+    width: 50px;
+    height: auto;
+    margin-left: 60px;
+    transition: all .2s ease-in-out;
+  }
+  button {
+  transition: transform .7s ease-in-out;}
+  .logo:hover {
+    transform: scale(1.5);
+  }
+  .menu-item:hover {
+    font-size: 110%;
+    border: 2px solid transparent;
+  }
+  .search-box {
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+    display: flex;
+    align-items: center; 
+  }
+  .input-search {
+    height: 50px;
+    width: 50px;
+    border-style: none;
+    font-size: 18px;
+    letter-spacing: 2px;
+    outline: none;
+    border-radius: 25px;
+    transition: all .5s ease-in-out;
+    background-color: #242326;
+    padding-right: 60px;
+    padding-left: 20px; 
+    color: #fff;
+    margin-right: 60px;
+  }
+  .input-search::placeholder {
+    color: rgba(255,255,255,.5);
+    font-size: 18px;
+    letter-spacing: 2px;
+    font-weight: 100;
+  }
+  .btn-search {
+    width: 50px;
+    height: 50px;
+    border-style: none;
+    font-size: 20px;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+    border-radius: 50%;
+    position: absolute;
+    right: 0; 
+    margin-left: 0; 
+    padding-right: 40px; 
+    color: #ffffff;
+    background-color: transparent;
+    margin-right: 50px;
+  }
+  .search {
+    width: 30px;
+    height: auto;
+    margin: 0;
+  }
+  .input-expanded {
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom: 1px solid rgba(255,255,255,.5);
+    margin-left: 60px;
+  }
+  h1 {
+  margin: 20px;
+}button:hover {
+   transform: rotate(360deg);
+
+}
 </style>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+  import { ref } from 'vue';
+
+  const isSearchExpanded = ref(false);
+
+  function toggleSearch() {
+    isSearchExpanded.value = !isSearchExpanded.value;
+  }
+</script>
